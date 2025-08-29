fixed gradient cclipping

p2_transformers/train.py
@@ -79,8 +79,6 @@ def train_loop(model, optimizer, tokens, loss_fn, scheduler_params, max_norm, ru
         writer.add_scalar("train/lr", optimizer.param_groups[0]['lr'], step)
         writer.add_scalar("train/loss", loss_acc, step)
         writer.add_scalar("train/perplexity", float('inf') if loss_acc > 20 else np.exp(loss_acc), step)
-        grad_norm = compute_grad_norm(model)
-        writer.add_scalar("train/grad_norm_before_clip", grad_norm, step)
 
         # log validate params and log in tensorboard
         if step % valid_every == 0:
@@ -96,16 +94,17 @@ def train_loop(model, optimizer, tokens, loss_fn, scheduler_params, max_norm, ru
 
         # clip gradients TODO: add to parser
         if max_norm is not None:
-            gradient_clipping(model.parameters(), max_l2_norm= max_norm)
-            grad_norm = compute_grad_norm(model)
-            writer.add_scalar("train/grad_norm_after_clip", grad_norm, step)
+            g_norm_pre, g_norm_post = gradient_clipping(model, max_l2_norm= max_norm)
+            writer.add_scalar("train/grad_norm/post_clip", g_norm_post, step)
+        else:
+            g_norm_pre = compute_grad_norm(model)
+        writer.add_scalar("train/grad_norm/pre_clip", g_norm_pre, step)
+            
         
-        # TODO: DELETE
-        # NOTE: test trust ratio
-        param_to_name = {param: name for name, param in model.named_parameters()}
-        # TODO: DELETE
-        optimizer.step(None, param_to_name) # TODO: DELETE
-
+        # TODO: DELETE after testing trust_ratio
+        # param_to_name = {param: name for name, param in model.named_parameters()}
+        # optimizer.step(None, param_to_name) # TODO: DELETE
+        optimizer.step()
         # log 'tokens per second'
         tokens_per_sec = os_bs * context_length / (perf_counter() - t_start)
         writer.add_scalar("train/tokens_per_sec", tokens_per_sec, step)
@@ -145,7 +144,7 @@ def main(config):
     # print intro
     curr_time = datetime.now().time()
     print()
-    optim_suffix = "_tr" if config["optimizer"]["is_trust_ratio"] else ""
+    optim_suffix = "_tr" if config["optimizer"]["name"] in {"Lion"} and config["optimizer"]["is_trust_ratio"] else ""
     optim_name = config["optimizer"]["name"] + optim_suffix
     print(
         f"Run started at {curr_time.strftime("%H:%M:%S")}: bs={run_params['batch_size']} | "
@@ -188,50 +187,25 @@ if __name__ == '__main__':
 
     with open(inputs.config, 'r') as stream:
         config = yaml.safe_load(stream)
-    for lr in [6e-3]: # TODO: add 6e-3
-        config["optimizer"]["lr"] = lr
-        config["optimizer"]["scheduler"]["lr_min"] = lr / 10
-        for is_trust_ratio in [False, True]:
-            config["optimizer"]["is_trust_ratio"] = is_trust_ratio
-            for optim_step_batch_size in [1280, 64]:
-                config["train"]["optim_step_batch_size"] = optim_step_batch_size
-                main(config)
-    # for lr_max in [5e-3, 1e-3]: # 1e+1, 1e-0, 1e-1, 5e-2, 1e-2, 1e-4
-    
-    # normal Lion
-    # config["train"]["batch_size"] = 64
-    # for lr in [1e-3, 5e-4, 3e-4,  1e-4]:
+
+    # testing AdamW
+    for weight_decay in [1e-2, 1e-3, 5e-4, 1e-4, 0]:
+        config["optimizer"]["weight_decay"] = weight_decay
+        for lr in [5e-3, 5e-4]:
+            config["optimizer"]["lr"] = lr
+            for lr_min in [1e-3, 1e-5, 1e-7]:
+                config["optimizer"]["scheduler"]["lr_min"] = lr_min
+                for optim_step_batch_size in [64]: #[1280, 64]:
+                    config["train"]["optim_step_batch_size"] = optim_step_batch_size
+                    main(config)
+
+    # testing trust ratio in Lion
+    # for lr in [6e-3]:
     #     config["optimizer"]["lr"] = lr
-    #     config["optimizer"]["scheduler"]["lr_min"] = lr * 1e-2
-    #     main(config)
-        
-    # Lion+trust_ratio
-    # config["optimizer"]["is_trust_ratio"] = True
-    # for bs in [64, 128, 192]:
-    #     config["train"]["batch_size"] = bs
-    #     for lr_max in [5e-3, 3e-3, 1e-3, 5e-4, 3e-4,  1e-4]:
-    #         config["optimizer"]["lr"] = lr_max
-    #         # for lr_min in [1e-3, 1e-4, 1e-5, 1e-6]: # , 1e-7
-    #         for ratio in [1e-1, 1e-2]:
-    #             lr_min = ratio * lr_max
-    #             config["optimizer"]["scheduler"]["lr_min"] = lr_min
+    #     config["optimizer"]["scheduler"]["lr_min"] = lr / 10
+    #     for is_trust_ratio in [True]: # [False, True]:
+    #         config["optimizer"]["is_trust_ratio"] = is_trust_ratio
+    #         for optim_step_batch_size in [64]: #[1280, 64]:
+    #             config["train"]["optim_step_batch_size"] = optim_step_batch_size
     #             main(config)
     
-    # if config["gpu"] == 0:
-    #     for lr in [10.0, 1.0, 1e-1, 1e-2, 5e-3, 1e-3, 1e-4]:
-    #         config["optimizer"]["lr"] = lr
-    #         for ratio in [1.0, 1e-1, 5e-2, 1e-2, 5e-3, 1e-3]:
-    #             config["optimizer"]["scheduler"]["lr_min"] = round(lr * ratio, 6)
-    #             main(config, inputs.log_structure)
-    # elif config["gpu"] == 1:
-    #     for lr in [10.0, 1.0, 1e-1, 1e-2]:
-    #         config["optimizer"]["lr"] = lr
-    #         for ratio in [1.0, 1e-1, 1e-2]:
-    #             config["optimizer"]["scheduler"]["lr_min"] = round(lr * ratio, 6)
-    #             main(config, inputs.log_structure)
-    # elif config["gpu"] == 1:
-    #     for lr in [7e-3, 4e-3, 1e-3]: # [5e-2, 3e-2, 1e-2, 5e-3]:
-    #         config["optimizer"]["lr"] = lr
-    #         for ratio in [2e-1, 1e-1, 5e-2]: #[1e-1, 5e-2]:
-    #             config["optimizer"]["scheduler"]["lr_min"] = round(lr * ratio, 6)
-    #             main(config, inputs.log_structure)

p2_transformers/utils/utils.py
@@ -34,16 +34,16 @@ def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tens
     return att
 
 def cross_entropy(logits: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-    # TODO: assert -x.dim() <= dim < x.dim()
-    # rearrange
-    logits_2d = rearrange(logits, "... v_size -> (...) v_size")
-    target_1d = rearrange(target, "... -> (...)")
-    # substract the largest value
-    adj_logits = logits_2d - logits_2d.max(dim=-1, keepdim=True).values
-    # calculate loss
-    exps = torch.exp(adj_logits)
-    B = adj_logits.shape[0]
-    losses = -adj_logits[torch.arange(B), target_1d] + torch.log(torch.sum(exps, dim = -1))
+    assert logits.shape[:-1] == target.shape, "logits and target shape dimension mismatch"
+    # Flatten all dimensions except vocab
+    logits_flat = rearrange(logits, "... v_size -> (...) v_size")
+    target_flat = rearrange(target, "... -> (...)")
+    # Numerical stability: subtract max logit per row
+    logits_adj = logits_flat - logits_flat.max(dim=-1, keepdim=True).values
+    # Calculate loss: Negative log-likelihood
+    exps = torch.exp(logits_adj)
+    B = logits_flat.shape[0]
+    losses = -logits_adj[torch.arange(B), target_flat] + torch.log(torch.sum(exps, dim = -1))
     return torch.mean(losses)
 
 def perplexity(logits: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
@@ -63,14 +63,14 @@ def cosine_lr_schedule(t: int, lr_max: float, lr_min: float, warmup_iters: int, 
         lr = lr_min
     return lr
 
-def gradient_clipping(params: Iterable[nn.Parameter], max_l2_norm: float, eps: float = 1e-6):
+def gradient_clipping(model, max_l2_norm: float, eps: float = 1e-6):
     """
-    returns pre-clipping gradient value for logging
+    returns pre-clipping and after-clipping gradient value for logging
     """
     assert max_l2_norm > 0, f"Max L2 norm should be positive but it is {max_l2_norm}."
     # get global norm
     sum_sq = None
-    for param in params:
+    for param in model.parameters():
         g = param.grad
         if g is not None:
             if sum_sq is None:
@@ -78,17 +78,20 @@ def gradient_clipping(params: Iterable[nn.Parameter], max_l2_norm: float, eps: f
             sum_sq += g.detach().float().pow(2).sum()
     # check if no gradients or current norm is too large
     if sum_sq is None:
-        return None
+        return None, None
     global_l2_norm = sum_sq.sqrt()
     if global_l2_norm <= max_l2_norm:
-        return global_l2_norm.item()
+        return global_l2_norm.item(), global_l2_norm.item()
     # update gradients
     scale = max_l2_norm / (global_l2_norm + eps)
+    grad_device = global_l2_norm.device
+    sum_sq = torch.zeros(1, device = grad_device)
     with torch.no_grad():
-        for param in params:
+        for param in model.parameters():
             if param.grad is not None:
                 param.grad.mul_(scale.to(param.grad.dtype))
-    return global_l2_norm.item()
+                sum_sq += param.grad.detach().float().pow(2).sum()
+    return global_l2_norm.item(), sum_sq.sqrt().item()
 
 def get_start_seqs(start_from: int | None, batch_size: int | None, x_len: int | None, in_memory_ids: np.ndarray | None, mode : str):
     assert mode in {"sample", "in_memory_ids"}
@@ -110,10 +113,6 @@ def data_loading(x: npt.NDArray, context_length: int, start_seqs: np.ndarray, de
 
     """
     # create masks to sample from numpy
-    # if start_from is not None:
-    #     start_seqs = np.arange(start_from, start_from + batch_size)[:, None]
-    # else:
-    #     start_seqs = random.randint(0, x.shape[0] - context_length, size=batch_size)[:, None] # NOTE: consider shuffle if I want without replacement
     steps_curr = np.arange(context_length)[None, :]
     steps_next = np.arange(1, context_length + 1)[None, :]
     mask_curr, mask_next = start_seqs + steps_curr, start_seqs + steps_next
@@ -198,7 +197,7 @@ def parse_optim(config_opt):
     }
     if config_opt["name"] in {"AdamW", "Adam"}:
         optimizer_params["betas"] = (config_opt["beta1"], config_opt["beta2"])
-        optimizer_params["eps"] = float(config_opt["epsilon"]),
+        optimizer_params["eps"] = float(config_opt["epsilon"])
         optimizer_params["decoupled"]: config_opt["name"] == "AdamW"
     elif config_opt["name"] == "Adan":
         optimizer_params["betas"] = (config_opt["beta1"], config_opt["beta2"], config_opt["beta3"])