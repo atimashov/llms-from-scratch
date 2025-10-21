1acdd11f2305ade37ce62046e70f14ebfba6acfc
Sasha Timashov <16296075+atimashov@users.noreply.github.com>
2025-10-21 15:50:07 +0100

    implemented MQA/GQA

--- p1_core/layers/attention.py
+++ p1_core/layers/attention.py
@@ -12,14 +12,32 @@ def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tens
     V:  (batch_size, ..., seq_len_kv, d_v)
     """
     d_qk = K.shape[-1]
-    # seq_len is the same for both, but I distinguish the ordering
-    scores = einsum(Q, K, "... seq_len_q d_qk, ... seq_len_kv d_qk -> ... seq_len_q seq_len_kv") 
-    scores = scores.clamp(min = -80, max=80.0)
+    H_q, H_kv = Q.shape[1], K.shape[1]
+    
+    # Grouped-Query Attention reshape
+    if H_q > H_kv and H_kv > 1: # GQA
+        Q = rearrange(Q, "... (h_kv r) seq_len_q d_qk -> ... h_kv r seq_len_q d_qk", h_kv = H_kv)
+        K = rearrange(K, "... h_kv seq_len_kv d_qk -> ... h_kv 1 seq_len_kv d_qk")
+        V = rearrange(V, "... h_kv seq_len_kv d_v -> ... h_kv 1 seq_len_kv d_v")
+    
+    # Compute scores
+    scores = einsum(Q, K, "... seq_len_q d_qk, ... seq_len_kv d_qk -> ... seq_len_q seq_len_kv")
+    scores = scores.clamp(min = -80, max=80.0) # NOTE: might be better after rescaling
+    
+    # Compute masking
     if mask is not None:
         scores = scores.masked_fill(~mask, float('-inf'))
+    
+    # Compute weights
     weights = softmax(scores / (d_qk ** 0.5), dim = -1)
-    att = einsum(weights, V, "... seq_len_q seq_len_kv, ... seq_len_kv d_v -> ... seq_len_q d_v")
-    return att
+    attn = einsum(weights, V, "... seq_len_q seq_len_kv, ... seq_len_kv d_v -> ... seq_len_q d_v")
+
+    # Rearrange to (batch_size, seq_len, h_q * d_v)
+    if H_q > H_kv and H_kv > 1: # GQA
+        attn = rearrange(attn, "... h_kv r seq_len_q d_v -> ... seq_len_q (h_kv r d_v)")
+    else: # MHA or MQA
+        attn = rearrange(attn, "... h_q seq_len_q d_v -> ... seq_len_q (h_q d_v)")
+    return attn
 
 class MultiHeadSelfAttention(nn.Module):
     """
@@ -28,45 +46,52 @@ class MultiHeadSelfAttention(nn.Module):
     TODO: check dtype/device correctness; add documentation;
     """
     def __init__(
-        self, d_model: int, num_heads: int, theta: float = 10000.0, context_length = 10000, max_len = 10000, kv_cache: bool = False,
+        self, d_model: int, num_heads: int, kv_heads_ratio: int = 1, theta: float = 10000.0, context_length = 10000, max_len = 10000, kv_cache: bool = False,
         init_type: str = 'xavier', clip_w: float = 3.0, device: torch.device | None = None, dtype: torch.dtype | None = None
         ):
         super().__init__()
         assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
-        self.d_model, self.num_heads = d_model, num_heads
-        self.d_k, self.d_v = d_model // num_heads, d_model // num_heads
-        # init projections
-        self.P_Q = Linear(d_model, num_heads * self.d_k, init_type, clip_w, device = device, dtype=dtype)
-        self.P_K = Linear(d_model, num_heads * self.d_k, init_type, clip_w, device = device, dtype=dtype)
-        self.P_V = Linear(d_model, num_heads * self.d_v, init_type, clip_w, device = device, dtype=dtype)
-        self.P_O = Linear(num_heads * self.d_v, d_model, init_type, clip_w, device = device, dtype=dtype)
-        # init RoPE
-        self.rope = RoPE(theta = theta, d_k = self.d_k, max_seq_len= context_length, device=device, dtype = dtype) if theta is not None else None
+        assert num_heads % kv_heads_ratio == 0, "'num_heads' should be divisible by 'kv_heads_ratio'"
+        self.num_heads_q = num_heads # queries
+        self.num_heads_kv = num_heads // kv_heads_ratio   # keys & values
+        self.d_model = d_model
+        self.d_qk = d_model // num_heads
+        self.d_v = d_model // num_heads
         self.device = device
-        # init KV Cache
+        
+        # Init Projections
+        self.P_Q = Linear(d_model, self.num_heads_q * self.d_qk, init_type, clip_w, device = device, dtype=dtype)
+        self.P_K = Linear(d_model, self.num_heads_kv * self.d_qk, init_type, clip_w, device = device, dtype=dtype)
+        self.P_V = Linear(d_model, self.num_heads_kv * self.d_v, init_type, clip_w, device = device, dtype=dtype)
+        self.P_O = Linear(self.num_heads_q * self.d_v, d_model, init_type, clip_w, device = device, dtype=dtype)
+        
+        # Init RoPE
+        self.rope = RoPE(theta = theta, d_k = self.d_qk, max_seq_len= context_length, device=device, dtype = dtype) if theta is not None else None
+        
+        # Init KV Cache
         if kv_cache: # TODO: check if dtype is correct for AMP
             # NOTE: for now it works only for batch_size = 1
             with torch.no_grad():
                 self.kv_cache = {
-                    "K": torch.zeros((1, num_heads, max_len, self.d_k), device = device, dtype = dtype),
-                    "V": torch.zeros((1, num_heads, max_len, self.d_v), device = device, dtype = dtype),
+                    "K": torch.zeros((1, self.num_heads_kv, max_len, self.d_qk), device = device, dtype = dtype),
+                    "V": torch.zeros((1, self.num_heads_kv, max_len, self.d_v), device = device, dtype = dtype),
                 }
     
     def get_proj_q(self, x):
         """
         Compute query (Q) projection.
 
-        if KV cache is enamled, it will return just projection on the last token.
+        if KV cache is enabled, it will return just projection on the last token.
         Otherwise - projection of all sequence.
 
         Args:
             x:  (batch_size, seq_len, d_model)
 
         Returns:
-            Q: (batch_size, seq_len, d_model) or (batch_size, 1, d_model)
+            Q: (batch_size, num_heads, seq_len, d_qk) or (batch_size, num_heads, 1, d_qk)
         """
         Q = self.P_Q(x[:,-1:,:] if hasattr(self, "kv_cache") else x)
-        return rearrange(Q, "... seq_len (h d_k) -> ... h seq_len d_k", h = self.num_heads)
+        return rearrange(Q, "... seq_len (h d_qk) -> ... h seq_len d_qk", h = self.num_heads_q)
 
     def get_proj_kv(self, x: torch.Tensor, name: str):
         """
@@ -93,31 +118,34 @@ class MultiHeadSelfAttention(nn.Module):
             assert batch_size == 1, f"Currently support batch_size = 1, but provided {batch_size}"
 
             proj_new = proj(x[:,-1:,:]) # batch_size x 1 x d_model
-            proj_new = rearrange(proj_new, "... seq_len (h d_proj) -> ... h seq_len d_proj", h = self.num_heads) # batch_size x num_heads x 1 x d_model
+            proj_new = rearrange(proj_new, "... seq_len (h d_proj) -> ... h seq_len d_proj", h = self.num_heads_kv) # batch_size x num_heads x 1 x d_model
             self.kv_cache[name][:, :, seq_len-1:seq_len,:] = proj_new
             out = self.kv_cache[name][:,:,:seq_len,:]
         else:
             out = proj(x)  # batch_size x seq_len x d_model
-            out = rearrange(out, "... seq_len (h d_proj) -> ... h seq_len d_proj", h = self.num_heads)
+            out = rearrange(out, "... seq_len (h d_proj) -> ... h seq_len d_proj", h = self.num_heads_kv)
         return out
 
     def forward(self, x: torch.Tensor, is_masked: bool = True, with_rope = True, token_positions = None):
-        # project x to get queries (Q), keys (K) and values
+        # Project x to get queries (Q), keys (K) and values
         Q = self.get_proj_q(x)  # batch_size x h x seq_len x d_qk  or batch_size x 1 x d_qk (KV cache)
         K = self.get_proj_kv(x, "K")  # batch_size x h x seq_len x d_qk
         V = self.get_proj_kv(x, "V")  # batch_size x h x seq_len x d_v
-        # apply RoPE
+        
+        # Apply RoPE
         if with_rope and self.rope is not None:
             Q = self.rope(Q)
             K = self.rope(K)
-        # create mask
+        
+        # Create mask
         if is_masked:
             mask = ~torch.triu(torch.full((Q.shape[-2], K.shape[-2]), True, device = self.device), diagonal=1)
         else:
             mask = None
-        # calculate scaled attention
+        
+        # Calculate scaled DPA
         scaled_mh_att = scaled_dot_product_attention(Q, K, V, mask)
-        scaled_mh_att = rearrange(scaled_mh_att, "... h seq_len d_v -> ... seq_len (h d_v)")
-        # project on output
-        O = self.P_O(scaled_mh_att) # O = einsum(scaled_mh_att, self.P_O, "... seq_len hd_v, d hd_v -> ... seq_len d")
+        
+        # Project an output
+        O = self.P_O(scaled_mh_att)
         return O
