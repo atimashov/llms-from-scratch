768272b5af83d97ca8b151598100f11f2cb9724f
Sasha Timashov <16296075+atimashov@users.noreply.github.com>
2025-11-03 00:26:36 +0000

    addded naive DDP implementation

--- p2_efficiency/multiprocessing/train.py
+++ p2_efficiency/multiprocessing/train.py
@@ -1,20 +1,16 @@
 import os
+from tqdm import tqdm
+from termcolor import colored
 import torch
 from torch import nn
 from torch.optim import Adam
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
-"""
-NOTE:
-When running GPU dostributed job, we need to ensure that 
-different ranks use different GPUS:
-- torch.cuda.set_device(rank) -> tensor.to('cuda')
-- device = f"cuda:{rank}"
-"""
 
 class ToyModel(nn.Module):
     def __init__(self, d_in: int, d_hidden: int, d_out: int):
+        super().__init__()
         self.W1 = nn.Linear(d_in, d_hidden)
         self.relu = nn.ReLU()
         self.W2 = nn.Linear(d_hidden, d_out)
@@ -28,30 +24,36 @@ class ToyModel(nn.Module):
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "29500"
+    torch.cuda.set_device(rank)
     # Init process grup: 'gloo' is used for CPU; 'nccl' - for machines with GPU
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
-def distributed_demo(rank, world_size):
-    setup(rank, world_size)
-    data = torch.randint(0, 10, (3,))
-    print(f"rank {rank} data (before all-reduce): {data}")
-    dist.all_reduce(data, async_op=False)
-    print(f"rank {rank} data (after all-reduce): {data}")
 
-
-def train_parallel(rank, world_size, dataset, model, loss_fn, opt, x, y, steps):
+def train_parallel(rank, world_size, batch_size, d_in, d_hidden, d_out): # , dataset, loss_fn, opt, x, y, steps):
     setup(rank, world_size)
 
-    device = torch.device(f"cuda:{rank}")
-    model.to(device)
+    # create the same model with random weights
+    model = ToyModel(d_in, d_hidden, d_out)
+    model.to("cuda")
+
+    # Broadcast parameters and buffers from rank "0"
+    with torch.no_grad():
+        for p in model.parameters():
+            dist.broadcast(p.data, src=0)
+        # Broadcast buffers (e.g., BatchNorm running stats)
+        for b in model.buffers():
+            dist.broadcast(b.data, src=0)
+
+    # init optimizer
+    opt = Adam(model.parameters())
+
+    # init loss function
+    loss_fn = nn.CrossEntropyLoss()
 
     # dataset
-    X, Y = dataset
-    MN = X.shape[0]
-    M = MN // world_size
-    x = X[rank * M: (rank + 1) * M]
-    y = Y[rank * M: (rank + 1) * M]
-    x, y = x.to(device), y.to(device)
+    x = torch.randn(batch_size, d_in)
+    y = torch.randint(0, d_out, (batch_size, ))
+    x, y = x.to("cuda"), y.to("cuda")
     
     loop = tqdm(
         range(1000),
@@ -74,30 +76,13 @@ def train_parallel(rank, world_size, dataset, model, loss_fn, opt, x, y, steps):
 
         opt.step()
 
-    
-   
-
-
 if __name__ == "__main__":
-    batch_size: int = 128, d_in: int: 64, d_hidden: int = 128, d_out: int = 16
+    batch_size: int = 128
+    d_in: int = 64
+    d_hidden: int = 128
+    d_out: int = 16
 
 
-    world_size = 4
-    # Create 'world_size' processes; each of them runs function fn (in out case 'distributed demo') with arguments (rank, *args)
+    world_size = 2
     
-
-    # init model
-    model = ToyModel(d_in, d_hidden, d_out) # TODO: and this?
-
-    # init optimizer
-    opt = Adam(model.parameters())
-
-    # init data
-    x = torch.randn(batch_size, d_in) # TODO: how are we going to process it here?
-    y = torch.randint(0, d_out, (batch_size, )) # TODO: how are we going to process it here?
-
-
-
-    loss_fn = nn.CrossEntropyLoss()
-    
-    mp.spawn(fn=distributed_demo, args=(world_size, ), nprocs=world_size, join=True)
+    mp.spawn(fn=train_parallel, args=(world_size, batch_size, d_in, d_hidden, d_out, ), nprocs=world_size, join=True)
